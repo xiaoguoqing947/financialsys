aa184a35ca90e4f1138025b85d6ea3daae2f57a9
Gumball <1159403411@qq.com>
2019-12-13 09:06:59 +0800

    细节修改

--- src/main/java/cn/xgq/financialsys/service/MenuSerImpl.java
+++ src/main/java/cn/xgq/financialsys/service/MenuSerImpl.java
@@ -4,15 +4,21 @@ import cn.xgq.financialsys.domain.Menu;
 import cn.xgq.financialsys.domain.dto.menu.UpdateMenuForm;
 import cn.xgq.financialsys.domain.vo.VoMenu;
 import cn.xgq.financialsys.mapping.MenuMapper;
+import cn.xgq.financialsys.redis.RedisService;
 import cn.xgq.financialsys.service.inter.MenuSer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class MenuSerImpl implements MenuSer {
@@ -20,10 +26,23 @@ public class MenuSerImpl implements MenuSer {
 
     @Resource
     private MenuMapper menuMapper;
+    @Resource
+    private RedisService redisService;
+    @Resource
+    private ValueOperations<String, Object> valueOperations;
 
     @Override
     public List<Menu> listMenu(Map<String, Object> searchMap) {
-        return menuMapper.findMenuList(searchMap);
+        /*redis缓存的使用*/
+        List<Menu> list = null;
+        if (redisService.existsKey("listMenu") && valueOperations.get("listMenu")!=null) {
+            list = (List<Menu>) valueOperations.get("listMenu");
+        } else {
+            list = menuMapper.findMenuList(searchMap);
+            valueOperations.set("listMenu", list, RedisService.DEFAULT_EXPIRE, TimeUnit.SECONDS);
+        }
+//        System.out.println(list);
+        return list;
     }
 
     @Override
@@ -44,6 +63,7 @@ public class MenuSerImpl implements MenuSer {
 
     @Override
     public boolean addMenu(Menu menu) {
+        redisService.deleteKey("listMenu");
         int num = 0;
         try {
             num = menuMapper.insertSelective(menu);
@@ -60,9 +80,10 @@ public class MenuSerImpl implements MenuSer {
 
     @Override
     public boolean updateMenu(UpdateMenuForm form) {
+        redisService.deleteKey("listMenu");
         int num = 0;
         try {
-            Menu menu=new Menu();
+            Menu menu = new Menu();
             menu.setId(form.getUpdateMenuId());
             menu.setName(form.getUpdateName());
             menu.setUrl(form.getUpdateUrl());
@@ -76,6 +97,7 @@ public class MenuSerImpl implements MenuSer {
 
     @Override
     public boolean deleteMenu(String id) {
+        redisService.deleteKey("listMenu");
         int num = 0;
         try {
             num = menuMapper.deleteByPrimaryKey(Integer.parseInt(id));
